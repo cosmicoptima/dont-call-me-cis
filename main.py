280f3abdc67fb146e968875e988389f9134f2e84
celeste <parafactual@gmail.com>
2022-12-30 17:46:43 -0800

    Switch to base davinci

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ class MyClient(discord.Client):
 
     async def set_name(self):
         completion = openai.Completion.create(
-            engine="text-davinci-003",
+            engine="davinci",
             prompt=f"Select a short phrase that DCMC would stand for. It should be a person's name, and indirectly allude to them being remarkably {self.adj1}, and also somewhat {self.adj2}.",
             temperature=0.65,
         )

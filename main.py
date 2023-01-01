e3450191673c5c72e5326f84cffd04996b4552ac
celeste <parafactual@gmail.com>
2023-01-01 13:25:53 -0800

    dcmc is becoming a mess

--- main.py
+++ main.py
@@ -43,7 +43,10 @@ class MyClient(discord.Client):
     adj1 = generate_adjective()
     adj2 = generate_adjective()
 
+    new_adjs = True
+
     shushed = False
+    in_progress = True
 
     async def set_avi(self):
         output_url = sd.predict(
@@ -74,11 +77,15 @@ class MyClient(discord.Client):
         body = "\n".join(
             f"{msg.author}: {msg.content}" for msg in messages[channel][-25:]
         )
+        if self.new_adjs:
+            final_sentence = f"Here, {self.name} shifts tone and vocabulary completely and instead exposes themselves as remarkably {self.adj1} and somewhat {self.adj2}."
+        else:
+            final_sentence = f"Here, {self.name} becomes slightly more {self.adj1} and {self.adj2} than in their previous message."
 
         return (
             "The following is a chatlog. The user '{self.name}' is insane and always replies in an unhinged manner.\n\n"
             + body
-            + f"\n(The following message is {'very short and ' if random.random() < 0.8 else ''}all lowercase. Here, {self.name} shifts tone and vocabulary completely and instead exposes themselves as remarkably {self.adj1} and somewhat {self.adj2}.)\n{self.name}:"
+            + f"\n(The following message is {'very short and ' if random.random() < 0.8 else ''}all lowercase. {final_sentence})\n{self.name}:"
         )
 
     async def on_ready(self):
@@ -86,6 +93,7 @@ class MyClient(discord.Client):
             await self.set_name()
         if not "--no-avi" in argv:
             await self.set_avi()
+        self.in_progress = False
 
     async def on_message(self, message):
         messages[message.channel].append(Message(message.author.name, message.content))
@@ -118,25 +126,30 @@ class MyClient(discord.Client):
             self.shushed = False
             return
 
-        p_factors = [
-            self.shushed,
-            "dcmc" in message.content,
-            message.channel.name == "do-converse-me-channel",
-        ]
+        if self.shushed or self.in_progress or message.author.id == self.user.id:
+            p = 0
+        else:
+            p_factors = [
+                "dcmc" in message.content,
+                message.channel.name == "do-converse-me-channel",
+            ]
 
-        match p_factors:
-            case [False, True, True]:
-                p = 1
-            case [False, True, False]:
-                p = 0.7
-            case [False, False, True]:
-                p = 0.4
-            case [False, False, False]:
-                p = 0
-            case [True, _, _]:
-                p = 0
+            match p_factors:
+                case [True, True]:
+                    p = 1
+                case [True, False]:
+                    p = 0.7
+                case [False, True]:
+                    p = 0.4
+                case [False, False]:
+                    p = 0
+
+        console.log("[b]RECEIVED MESSAGE[/b]", f"[yellow]p={p}")
 
         if random.random() < p:
+            self.in_progress = True
+            await asyncio.sleep(random.random() * 2 + 0.2)
+
             async with message.channel.typing():
                 completion = openai.Completion.create(
                     engine="text-davinci-003",
@@ -148,15 +161,26 @@ class MyClient(discord.Client):
                     presence_penalty=0.5,
                 )
                 text = completion.choices[0].text.strip()
-                await asyncio.sleep(len(text) / 10)
-                await message.channel.send(text)
 
-                console.log("[b]POSTED[/b]", f"[green]{text}[/green]", f"[yellow]{self.adj1}[/yellow]", f"[yellow]{self.adj2}[/yellow]")
+                await asyncio.sleep(len(text) / 10)
+
+                if len(text) > 0:
+                    await message.channel.send(text)
+                    console.log("[b]POSTED[/b]", f"[green]{text}[/green]", f"[yellow]{self.adj1}[/yellow]", f"[yellow]{self.adj2}[/yellow]")
+                else:
+                    console.log("[b]DID NOT POST[/b]", "[red]empty message[/red]")
+
+            self.in_progress = False
+            self.new_adjs = False
 
         if random.random() < 0.02:
             self.adj1 = generate_adjective()
+            self.new_adjs = True
+            console.log("[b]NEW ADJ1[/b]", f"[yellow]{self.adj1}[/yellow]")
         if random.random() < 0.1:
             self.adj2 = generate_adjective()
+            self.new_adjs = True
+            console.log("[b]NEW ADJ2[/b]", f"[yellow]{self.adj2}[/yellow]")
 
         if random.random() < 0.05:
             self.shushed = False
